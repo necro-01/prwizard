f094d6b055b4f7aeaa16ef622611af24c3c101de
necro <prit.suvagiya01@gmail.com>
2023-07-02 15:59:13 +0530

    Added baseCommand which provides _run & spinner for all commands

---
+++ src/commands/base.command.ts
@@ -0,0 +1,27 @@
+import ora from 'ora';
+
+import { CommandConfig } from '../interfaces';
+import { logger } from '../logger';
+import chalk from "chalk";
+
+export abstract class BaseCommand<T> {
+    protected config: CommandConfig;
+    protected spinner: ora.Ora;
+
+    protected constructor(config: CommandConfig) {
+        this.config = config;
+        this.spinner = ora();
+    }
+
+    protected abstract _run(args?: T): Promise<void>;
+
+    public async run(args?: T): Promise<void> {
+        try {
+            await this._run(args);
+        } catch (error: any) {
+            this.spinner.stop();
+            logger.error(chalk.bgRedBright(error.message));
+            process.exit(1);
+        }
+    }
+}
